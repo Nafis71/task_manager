2d1a1ce02715c6e816c2ba341076453a556d8655
Nafis71 <nhtonmoy2@gmail.com>
2024-06-03 01:52:21 +0600

    signIn screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:device_preview_minus/device_preview_minus.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager/app/app.dart';
 
 main(){
-  runApp(const TaskManager());
+  runApp(DevicePreview(builder: (_) => const TaskManager()));
 }
---
+++ lib/views/signUpScreen/sign_up_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
