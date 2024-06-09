4a3beeb37b465c6ca3443a2640457b196a37d21d
Nafis71 <nhtonmoy2@gmail.com>
2024-06-09 07:52:44 +0600

    New task screen added

--- lib/viewModels/task_view_model.dart
+++ lib/viewModels/task_view_model.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/foundation.dart';
 import 'package:task_manager/models/responseModel/success.dart';
+import 'package:task_manager/models/taskListModel/task_data.dart';
+import 'package:task_manager/models/taskListModel/task_list_model.dart';
 import 'package:task_manager/models/taskStatusCountModels/task_status_count_model.dart';
 import 'package:task_manager/services/task_service.dart';
 import '../models/taskStatusCountModels/status_data.dart';
 
 class TaskViewModel extends ChangeNotifier{
   List<StatusData> _taskStatusData =[];
+  Map<String, List<TaskData>> _taskDataByStatus = {};
   Map<String,String> taskStatusCount ={};
   bool _isLoading = false;
+  bool _shouldRefresh = false;
   bool status = false;
   late Object response;
   TaskService taskService = TaskService();
   bool get isLoading => _isLoading;
+  bool get shouldRefresh => _shouldRefresh;
 
-  void setIsLoading(bool value){
+  void setShouldRefresh(bool value){
     _isLoading = value;
+    _shouldRefresh = value;
     notifyListeners();
   }
 
 
-  List<StatusData> get taskStatusData => _taskStatusData;
 
-  Future<bool> fetchTaskStatusData(String token) async{
-    status = false;
+  List<StatusData> get taskStatusData => _taskStatusData;
+  Map<String, List<TaskData>> get taskDataByStatus => _taskDataByStatus;
+
+  Future<void> fetchTaskStatusData(String token) async{
     response = await taskService.fetchTaskStatusCount(token);
     if(response is Success){
       TaskStatusCountModel taskStatusCountModel = (response as Success).response as TaskStatusCountModel;
@@ -34,10 +41,19 @@ class TaskViewModel extends ChangeNotifier{
           }
         }
       }
-      setIsLoading(false);
-      status = true;
     }
-    return status;
+  }
+
+  Future<void> fetchTaskList(String token, String taskStatus) async{
+    status = false;
+    response = await taskService.fetchTaskList(taskStatus, token);
+    if(response is Success){
+      TaskListModel taskListModel = (response as Success).response as TaskListModel;
+      if(taskListModel.taskData != null && taskListModel.taskData!.isNotEmpty){
+        _taskDataByStatus[taskStatus] = List.from(taskListModel.taskData as Iterable);
+        setShouldRefresh(false);
+      }
+    }
   }
 
 
---
+++ lib/views/widgets/task_list_card.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ionicons/ionicons.dart';
+
+import '../../models/taskListModel/task_data.dart';
+import '../../utils/app_color.dart';
+
+class TaskListCard extends StatelessWidget {
+  final double screenWidth;
+  final List<TaskData> taskData;
+
+  const TaskListCard(
+      {super.key,
+      required this.screenWidth,
+      required this.taskData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.separated(
+          itemBuilder: (context, index) {
+            return SizedBox(
+              width: screenWidth,
+              child: Card(
+                elevation: 0,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 18.0, vertical: 13.00),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        taskData[index].title.toString(),
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                      SizedBox(
+                        width: screenWidth * 0.8,
+                        child: Text(
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.justify,
+                          taskData[index].description.toString(),
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                      ),
+                      const Gap(10),
+                      Text(
+                        taskData[index].createdDate.toString().replaceAll("-", "/"),
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      const Gap(5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Chip(
+                            label: Text(taskData[index].status.toString()),
+                            backgroundColor: AppColor.newTaskChipColor,
+                            labelPadding: const EdgeInsets.symmetric(
+                                horizontal: 34, vertical: 0),
+                            labelStyle: const TextStyle(
+                              color: AppColor.newTaskChipTextColor,
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.edit_note_outlined,
+                                    color: AppColor.editIconColor,
+                                  )),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Ionicons.trash,
+                                  color: AppColor.deleteIconColor,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) {
+            return const Gap(5);
+          },
+          itemCount: taskData.length),
+    );
+  }
+}
