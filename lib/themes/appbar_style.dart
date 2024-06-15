e55b229ad3f9a486cbf6de5a1ef43e04be6899f2
Nafis71 <nhtonmoy2@gmail.com>
2024-06-16 00:37:47 +0600

    appbar elevation removed

--- lib/themes/appbar_style.dart
+++ lib/themes/appbar_style.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:task_manager/utils/app_color.dart';
 
 class AppbarStyle{
-  static AppBarTheme getAppbarStyle() => AppBarTheme(
+  static AppBarTheme getAppbarStyle() => const AppBarTheme(
     backgroundColor: AppColor.appBarBackgroundColor,
     foregroundColor: AppColor.appBarForegroundColor,
     surfaceTintColor: AppColor.appBarBackgroundColor,
+    elevation: 0
   );
 }
