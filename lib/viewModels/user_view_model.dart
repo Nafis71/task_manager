05fe4ad7867f127039d0a085c405adec68e2a4bb
Nafis71 <nhtonmoy2@gmail.com>
2024-06-24 03:48:54 +0600

    photo upload bug fixed

--- lib/viewModels/user_view_model.dart
+++ lib/viewModels/user_view_model.dart
@@ -63,7 +63,7 @@ class UserViewModel extends ChangeNotifier {
   }) async {
     setIsLoading = true;
     SharedPreferences preferences = await SharedPreferences.getInstance();
-    if (preferences.getString("photo")!.isNotEmpty && base64Image.isEmpty) {
+    if (_userData.photo!.isNotEmpty && base64Image.isEmpty) {
       base64Image = preferences.getString("photo").toString();
     }
     UserData userData = UserData(
